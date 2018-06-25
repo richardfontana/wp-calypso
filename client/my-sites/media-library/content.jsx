129aa16b5a6984714ff6f831ed8825137e800b91
jakejones1984 <40353716+jakejones1984@users.noreply.github.com>
2018-06-25 22:34:50 +0100

    Fix Date Filters to show only when enabled
    Add both a config feature flag and prop to determine whether to show the Date Filters. Append `?flags=external-media/google-photos/date-filters` to enable the feature for testing

--- client/my-sites/media-library/content.jsx
+++ client/my-sites/media-library/content.jsx
@@ -299,6 +299,7 @@ class MediaLibraryContent extends React.Component {
 					sticky={ ! this.props.scrollable }
 					hasAttribution={ 'pexels' === this.props.source }
 					hasRefreshButton={ 'pexels' !== this.props.source }
+					hasDateFilters={ 'google_photos' === this.props.source }
 				/>
 			);
 		}
