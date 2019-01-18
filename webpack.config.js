c225570a911fafe14115f7fabdfd98acf33f765a
Ben Lowery <ben.lowery@automattic.com>
2019-01-18 10:26:45 -0500

    Split off the babel config for things in node_modules. Teach that config to detect the module type. Teach webpack about the new config.

--- webpack.config.js
+++ webpack.config.js
@@ -211,9 +211,6 @@ function getWebpackConfig( { cssFilename, externalizeWordPressPackages = false }
 			],
 		},
 		module: {
-			// avoids this warning:
-			// https://github.com/localForage/localForage/issues/577
-			noParse: /[\/\\]node_modules[\/\\]localforage[\/\\]dist[\/\\]localforage\.js$/,
 			rules: [
 				{
 					test: /\.jsx?$/,
@@ -250,7 +247,7 @@ function getWebpackConfig( { cssFilename, externalizeWordPressPackages = false }
 						{
 							loader: 'babel-loader',
 							options: {
-								configFile: path.resolve( __dirname, 'babel.config.js' ),
+								configFile: path.resolve( __dirname, 'babel.dependencies.config.js' ),
 								babelrc: false,
 								cacheDirectory: path.join( __dirname, 'build', '.babel-client-cache' ),
 								cacheIdentifier,
